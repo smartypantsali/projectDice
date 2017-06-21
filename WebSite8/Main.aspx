2d4c37705ab4f94b46aeb191b22fad0560958500
smartypantsali <aligorur1994@hotmail.co.uk>
2017-06-21 16:16:59 +0100

    Add files via upload

---
+++ WebSite8/Main.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Main.aspx.cs" Inherits="Main" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title></title>   
+    <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Patrick+Hand+SC" rel="stylesheet"> 
+    <link href="Content/bootstrap.min.css" rel="stylesheet" />
+     <link href="Content/StyleSheet.css" rel="stylesheet" />     
+</head>
+<body>
+    <form id="form1" runat="server">      
+    <div class="BackgroundDice">
+    <section>
+        <div class="BorderuL">           
+         <div class="InnerBorder">             
+         <div class="Links"> 
+         <asp:Label CssClass="GameTitle" ID="GameTitle" runat="server" Text="Dice Roll Game"></asp:Label>
+         <asp:Button CssClass="StartGame" ID="Button1" runat="server" Text="Start Game" OnClick="Button1_Click" />                    
+         </div>
+                        <div class="GameClass">
+                  <asp:Panel CssClass="GamePanel" ID="PanelGame" runat="server" Visible="false">                      
+                      <asp:Button CssClass="DiceRoll" ID="DiceRoll" runat="server" Text="Click me to roll" OnClick="DiceRoll_Click" /> <br />  
+                      <asp:Label CssClass="GameText" ID="DieRollText" runat="server"></asp:Label><asp:Image CssClass="RolledDice" ID="ImageRoll" runat="server"/><br />
+                      <asp:Label CssClass="GameText" ID="DieRollTotal" runat="server"></asp:Label><br />  
+                      <asp:Label CssClass="GameText" ID="DisplayResult" runat="server"></asp:Label><br /> 
+                      <asp:Button CssClass="NewGameButton" ID="NewGameButton" runat="server" Text="New Game" OnClick="NewGameButton_Click" /><br />  
+                      <asp:Button CssClass="GameButton" ID="ButtonGameClose" runat="server" Text="Main Menu" OnClick="ButtonGameClose_Click" /><br />  
+                    </asp:Panel>
+             </div>
+          <div class="Links2">
+                <asp:Button CssClass="Instructions" ID="Button2" runat="server" Text="Instructions" OnClick="Button2_Click" />  
+          </div>   
+            <div class="InstructionsClass">
+                 <asp:Panel CssClass="InstructionsPanel" ID="PanelInstructions" runat="server" Visible="false">
+                     <asp:Label CssClass="InstructionsLabe1" ID="InstructionLabel1" runat="server" Text="Instructions:"></asp:Label> <br />
+                     <asp:Label CssClass="InstructionsLabel" ID="LabelInstructions" runat="server" Text="The objective of this game is to obtain 
+                         a total score of 21. This is <br /> achieved by clicking the 'Click me to roll' button. After clicking <br /> it consecutively your score will then
+                         be accumilated and displayed, <br/> if you happen to recieve a score of 21 you win, if its above 21 then you lose."> </asp:Label><br/> <br/>
+                     <asp:Button CssClass="InstructionsButton" ID="ButtonPanelInstructions" runat="server" Text="Main Menu" OnClick="ButtonPanelInstructions_Click" />                     
+                 </asp:Panel>
+                </div>
+                
+                  </div>                                                         
+          </div>          
+          
+
+    </section>
+        </div>
+        </form>
+</body>
+</html>
---
+++ WebSite8/Main.aspx.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Main : System.Web.UI.Page
+{
+    //Dice list
+    static List<int> diceList = new List<int>();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+       if(!Page.IsPostBack)
+       {
+            PanelGame.Visible = false;
+            PanelInstructions.Visible = false;
+        }
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        GameTitle.Visible = false;
+        PanelGame.Visible = true;
+        Button1.Visible = false;
+        Button2.Visible = false;
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        GameTitle.Visible = false;
+        PanelInstructions.Visible = true;
+        Button1.Visible = false;
+        Button2.Visible = false;
+    }
+
+    protected void ButtonPanelInstructions_Click(object sender, EventArgs e)
+    {
+        GameTitle.Visible = true; 
+        PanelInstructions.Visible = false;
+        Button1.Visible = true;
+        Button2.Visible = true;
+    }
+
+    protected void ButtonGameClose_Click(object sender, EventArgs e)
+    {
+        GameTitle.Visible = true;
+        PanelGame.Visible = false;
+        Button1.Visible = true;
+        Button2.Visible = true;
+        diceList.Clear();
+        DiceRoll.Enabled = true;
+        DieRollText.Text = "";
+        DisplayResult.Text = "";
+        DieRollTotal.Text = "";
+        ImageRoll.ImageUrl = "";
+    }
+
+    protected void DiceRoll_Click(object sender, EventArgs e)
+    {
+        int sum = 0; 
+
+        //Generate random number
+        Random randomNumber = new Random();
+        int randomRoll = randomNumber.Next(1, 7);
+
+        switch (randomRoll)
+        {
+            case (1):
+                DieRollText.Text = "You rolled a: " + Convert.ToString(randomRoll) + " ";
+                ImageRoll.ImageUrl = "~/Terning1.svg.png";
+                break;
+            case (2):
+                DieRollText.Text = "You rolled a: " + Convert.ToString(randomRoll) + " ";
+                ImageRoll.ImageUrl = "~/Terning2.svg.png";
+                break;
+            case (3):
+                DieRollText.Text = "You rolled a: " + Convert.ToString(randomRoll) + " ";
+                ImageRoll.ImageUrl = "~/Terning3.svg.png";
+                break;
+            case (4):
+                DieRollText.Text = "You rolled a: " + Convert.ToString(randomRoll) + " ";
+                ImageRoll.ImageUrl = "~/Terning4.svg.png";
+                break;
+            case (5):
+                DieRollText.Text = "You rolled a: " + Convert.ToString(randomRoll) + " ";
+                ImageRoll.ImageUrl = "~/Terning5.svg.png";
+                break;
+            case (6):
+                DieRollText.Text = "You rolled a: " + Convert.ToString(randomRoll) + " ";
+                ImageRoll.ImageUrl = "~/Terning6.svg.png";
+                break;
+        }
+        //Add randomRoll to List.
+        diceList.Add(randomRoll);
+        //Loop through List.
+        foreach (int i in diceList)
+        {
+            sum += i;
+        }        
+        if(sum >= 21)
+            {
+                DiceRoll.Enabled = false;                
+            }  
+        if(sum == 21)
+        {
+            DisplayResult.Text = "Congratulations you win!! Click New Game to play again.";
+        }
+        else if(sum > 21)
+        {
+            DisplayResult.Text = "Too bad, you lost :(. Click New Game to try again.";
+        }
+
+        //Convert to string to display as text.
+        DieRollTotal.Text = "Total: " + Convert.ToString(sum);
+    }
+
+
+    protected void NewGameButton_Click(object sender, EventArgs e)
+    {
+        diceList.Clear();
+        DiceRoll.Enabled = true;
+        DieRollText.Text = "";
+        DisplayResult.Text = "";
+        DieRollTotal.Text = "";
+        ImageRoll.ImageUrl = "";
+
+    }
+}
